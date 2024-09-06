23e067acb82cf809786e732f2f5b3ec83eadef02
EliJohns268 <elijohns@udel.edu>
2024-09-06 19:30:28 -0400

    Update Hello World Eli J

--- Homework Project/Assets/Scripts/Hello World.cs
+++ Homework Project/Assets/Scripts/Hello World.cs
@@ -7,7 +7,15 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!");   
+        Debug.Log("Hello, World!");
+        Debug.LogWarning("Erm, What the Sigma?");
+        Debug.LogError("Dear God No!");
+        //Author: Eli Johnson
+        /*
+         * I guess a question I have is how one would go about coding Grappling Hook physics.
+         * This relates to my game idea.
+         * The idea is a movement based shooter either pvp based or just a single-player target shooting course type game, or both. The Grappling physics would be similar to thoses in the Anime attack on titan if you've seen that.
+         */
     }
 
     // Update is called once per frame
