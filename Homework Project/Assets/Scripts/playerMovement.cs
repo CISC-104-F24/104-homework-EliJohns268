b34b72dde3259f41974f9eec284efdcc6460dd54
EliJohns268 <elijohns@udel.edu>
2024-10-18 17:39:19 -0400

    Finished HW2

--- Homework Project/Assets/Scripts/playerMovement.cs
+++ Homework Project/Assets/Scripts/playerMovement.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class playerMovement : MonoBehaviour
 {
+
+
+    public float rotationSpeed = 100f;
+    public float sprintSpeed = 15f;
     public float movementSpeed = 5.0f;
     public float jumpPower = 10f;
     Vector3 movementDirectionW = Vector3.forward;
@@ -14,16 +19,52 @@ public class playerMovement : MonoBehaviour
     public KeyCode KeyB = KeyCode.None;
     public KeyCode KeyL = KeyCode.None;
     public KeyCode KeyR = KeyCode.None;
-
+    public KeyCode Space = KeyCode.None;
     // Start is called before the first frame update
     void Start()
     {
         
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+
+
+        bool isjumpPressed = Input.GetKeyDown(KeyCode.Space);
+        if (isjumpPressed)
+        {
+            Rigidbody myRb = GetComponent<Rigidbody>();
+            myRb.AddForce(new Vector3(0, 10, 0) * jumpPower, ForceMode.Impulse);
+        }
+
+        bool isSprintKeyPressed = Input.GetKey(KeyCode.LeftShift);
+        if (isSprintKeyPressed)
+        {
+            movementSpeed = 15f;
+            
+            
+        }
+        else
+        {
+            movementSpeed = 5f;
+        }
+
+       
+
+
       bool isKeyPressedF = Input.GetKey(KeyF);
         if (isKeyPressedF)
         {
@@ -49,4 +90,10 @@ public class playerMovement : MonoBehaviour
         }
 
     }
+ 
+
+
+
+
+
 }
