55e408038fdce3c69e62d32627dcd8673517b55c
EliJohns268 <elijohns@udel.edu>
2024-12-11 20:38:25 -0500

    HW4 submit

---
+++ Homework Project/Assets/Scripts/Card.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+    public int hitpoints = 100;
+    public int hitPercent = 60;
+    public string rangerSequoia;
+    public int attackDMG = 40;
+    public TextMeshProUGUI Vats;
+    public TextMeshProUGUI HP;
+    public TextMeshProUGUI RangerSequoia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
